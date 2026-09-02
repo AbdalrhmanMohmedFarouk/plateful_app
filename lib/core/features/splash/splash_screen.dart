b6c39e425e863af2a2c3688735686e23281a5985
Abdalrhman Mohmed <abdalrhmanmohmed750@gmail.com>
2026-09-02 19:01:06 +0300

    finish Splash Screen

---
+++ lib/core/features/splash/splash_screen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:plateful/core/features/auth/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(
+      const Duration(seconds: 3),
+          () {
+        if (!mounted) return;
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) {
+              return LoginScreen();
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(
+                'assets/splash/backGround.png',
+                fit: BoxFit.cover,
+                color: Colors.grey,
+                colorBlendMode: BlendMode.saturation,
+              ),
+            ),
+
+            Center(
+              child: Image.asset(
+                "assets/splash/burger.png",
+                width: 250,
+                height: 400,
+              ),
+            ),
+
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 41),
+                child: Column(
+                  children: [
+                    Spacer(),
+
+                    Text(
+                      "Plateful",
+                      style: TextStyle(
+                        fontSize: 61,
+                        color: Color(0xFFFFCE52),
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'FleurDeLeah',
+                      ),
+                    ),
+
+                    Text(
+                      "MEAL PLANNER",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Color(0xFFFFFFFF),
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'NotoSans',
+                      ),
+                    ),
+
+                    SizedBox(height: 42),
+
+                    Text(
+                      "Your meal planning,\nserved on a full plate.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color(0xE5FFFFFF),
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'CormorantInfant',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plateful/core/features/splash/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', theme: ThemeData(),
-
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: SplashScreen(),
     );
-
   }
 }
